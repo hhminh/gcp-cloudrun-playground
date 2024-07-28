afe9a21fe28a3ce609c6ebf03f3e5e902e002e97
hhminh <hhminh@hotmail.com>
2024-07-28 19:47:03 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "${var.tf_org}"
+    organization = "eleetgroup"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-       name = "${var.tf_workspace}"
+       name = "playground"
     }
   }
 }
